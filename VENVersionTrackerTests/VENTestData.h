06a6055ce630718b381634cb405b9be61c40e389
Chris Maddern <chris@venmo.com>
2013-11-11 10:20:51 -0800

    Add tests for dev versions being considered above the respective release version

--- VENVersionTrackerTests/VENTestData.h
+++ VENVersionTrackerTests/VENTestData.h
@@ -1,11 +1,13 @@
 #define VEN_TEST_INSTALL_URL_1_1     @"http://install_url_1_1"
 #define VEN_TEST_INSTALL_URL_1_2_a   @"http://install_url_1_2_a"
+#define VEN_TEST_INSTALL_URL_1_2     @"http://install_url_1_2"
 #define VEN_TEST_INSTALL_URL_1_2_a2  @"http://install_url_1_2_a2"
 #define VEN_TEST_INSTALL_URL_1_2_b1  @"http://install_url_1_2_b1"
 #define VEN_TEST_INSTALL_URL_1_0_1   @"http://install_url_1_0_1"
 #define VEN_TEST_INSTALL_URL_2_0     @"http://install_url_2_0"
 
 #define VEN_TEST_VERSION_NAME_1_1       @"1.1.0"
+#define VEN_TEST_VERSION_NAME_1_2       @"1.2.0"
 #define VEN_TEST_VERSION_NAME_1_2_a     @"1.2.0a1"
 #define VEN_TEST_VERSION_NAME_1_2_a2    @"1.2.0a2"
 #define VEN_TEST_VERSION_NAME_1_2_b1    @"1.2.0b1"
@@ -13,6 +15,7 @@
 #define VEN_TEST_VERSION_NAME_2_0       @"2.0.0"
 
 #define VEN_TEST_VERSION_DICT_1_1       @{@"number": VEN_TEST_VERSION_NAME_1_1, @"install_url": VEN_TEST_INSTALL_URL_1_1, @"mandatory": @(NO)}
+#define VEN_TEST_VERSION_DICT_1_2     @{@"number": VEN_TEST_VERSION_NAME_1_2, @"install_url": VEN_TEST_INSTALL_URL_1_2, @"mandatory": @(NO)}
 #define VEN_TEST_VERSION_DICT_1_2_a     @{@"number": VEN_TEST_VERSION_NAME_1_2_a, @"install_url": VEN_TEST_INSTALL_URL_1_2_a, @"mandatory": @(NO)}
 #define VEN_TEST_VERSION_DICT_1_2_a2    @{@"number": VEN_TEST_VERSION_NAME_1_2_a2, @"install_url": VEN_TEST_INSTALL_URL_1_2_a2, @"mandatory": @(NO)}
 #define VEN_TEST_VERSION_DICT_1_2_b1    @{@"number": VEN_TEST_VERSION_NAME_1_2_b1, @"install_url": VEN_TEST_INSTALL_URL_1_2_b1, @"mandatory": @(NO)}
--- VENVersionTrackerTests/VENVersionTests.m
+++ VENVersionTrackerTests/VENVersionTests.m
@@ -22,13 +22,13 @@
 
 
 - (void)testVersionCreation {
-    VENVersion *version2_0      = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_2_0];
-    VENVersion *version1_1      = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_1_1];
-    VENVersion *version1_2_0_a2 = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_1_2_a2];
-    VENVersion *version1_2_0_b1 = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_1_2_b1];
+    VENVersion *version2_0      = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_2_0];
+    VENVersion *version1_1      = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_1_1];
+    VENVersion *version1_2_0_a2 = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_1_2_a2];
+    VENVersion *version1_2_0_b1 = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_1_2_b1];
     
-    VENVersion *version_inv_empty   = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_INV_EMPTY];
-    VENVersion *version_inv_types   = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_INV_TYPES];
+    VENVersion *version_inv_empty   = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_INV_EMPTY];
+    VENVersion *version_inv_types   = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_INV_TYPES];
     
     XCTAssertNil(version_inv_empty, @"Created Invalid Version");
     XCTAssertNil(version_inv_types, @"Created Invalid Version");
@@ -48,12 +48,13 @@
 }
 
 - (void)testVersionComparison {
-    VENVersion *version2_0      = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_2_0];
-    VENVersion *version1_1      = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_1_1];
-    VENVersion *version1_0_1    = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_1_0_1];
-    VENVersion *version1_2_0_a  = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_1_2_a];
-    VENVersion *version1_2_0_a2 = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_1_2_a2];
-    VENVersion *version1_2_0_b1 = [[VENVersion alloc] initWithJSONPayload:VEN_TEST_VERSION_DICT_1_2_b1];
+    VENVersion *version2_0      = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_2_0];
+    VENVersion *version1_1      = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_1_1];
+    VENVersion *version1_0_1    = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_1_0_1];
+    VENVersion *version1_2_0    = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_1_2];
+    VENVersion *version1_2_0_a  = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_1_2_a];
+    VENVersion *version1_2_0_a2 = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_1_2_a2];
+    VENVersion *version1_2_0_b1 = [[VENVersion alloc] initWithDictionary:VEN_TEST_VERSION_DICT_1_2_b1];
     
     // Check Descending Comparisons
     XCTAssertEqual([version2_0 compare:version1_0_1], NSOrderedDescending, @"Comparison returned unexpected result");
@@ -88,6 +89,14 @@
     XCTAssertEqual([version1_2_0_a2 compare:version1_2_0_a2], NSOrderedSame, @"Comparison returned unexpected result");
     XCTAssertEqual([version1_2_0_b1 compare:version1_2_0_b1], NSOrderedSame, @"Comparison returned unexpected result");
     XCTAssertEqual([version2_0 compare:version2_0], NSOrderedSame, @"Comparison returned unexpected result");
+    
+    // Check release version (no added letters / numbers) are higher than any b / a / rc builds
+    XCTAssertEqual([version1_2_0_a compare:version1_2_0], NSOrderedAscending, @"Incorrectly considered development build above production build");
+    XCTAssertEqual([version1_2_0_b1 compare:version1_2_0], NSOrderedAscending, @"Incorrectly considered development build above production build");
+    XCTAssertEqual([version1_2_0_a2 compare:version1_2_0], NSOrderedAscending, @"Incorrectly considered development build above production build");
+    XCTAssertEqual([version1_2_0 compare:version1_2_0_a], NSOrderedDescending, @"Incorrectly considered development build above production build");
+    XCTAssertEqual([version1_2_0 compare:version1_2_0_b1], NSOrderedDescending, @"Incorrectly considered development build above production build");
+    XCTAssertEqual([version1_2_0 compare:version1_2_0_a2], NSOrderedDescending, @"Incorrectly considered development build above production build");
     
 }
 
